3fcb6859bec8ed1d8f0751006db63781f52edda2
tustin2121 <tustin2121@gmail.com>
2019-09-18 18:58:00 -0400

    Removed latent {KUN}s

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1740,7 +1740,7 @@ gUnknown_08272A3F:: @ 8272A3F
 	.string "Please come again!$"
 
 gUnknown_08272A52:: @ 8272A52
-	.string "{PLAYER}{STRING 5}, welcome!\pWhat can I do for you?$"
+	.string "{PLAYER}, welcome!\pWhat can I do for you?$"
 
 gText_ObtainedTheItem:: @ 8272A78
 	.string "Obtained {STR_VAR_1} {STR_VAR_2}!$"
