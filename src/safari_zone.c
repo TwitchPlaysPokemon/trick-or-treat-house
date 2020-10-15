1cf5001098860c6822e08f15eb183563521d41ae
tustin2121 <tustin2121@gmail.com>
2020-10-14 20:34:13 -0400

    More bugfixes
    - Also upping the safari step count limit to allow the player to actually grab all the items inside.

--- src/safari_zone.c
+++ src/safari_zone.c
@@ -58,7 +58,7 @@ void EnterSafariMode(void)
     SetSafariZoneFlag();
     ClearAllPokeblockFeeders();
     gNumSafariBalls = 30;
-    sSafariZoneStepCounter = 500;
+    sSafariZoneStepCounter = 984; //500;
     sSafariZoneCaughtMons = 0;
     sSafariZonePkblkUses = 0;
 }
